f11d898d1e33ea9adb293473a2c10a653b7fcc5b
pdharsha <pdharsha@github.com>
2021-05-08 09:26:45 +0530

    initial

---
+++ src/main/java/covid/tracker/stats/LocationStats.java
@@ -0,0 +1,28 @@
+package covid.tracker.stats;
+
+public class LocationStats {
+    
+    private String country;
+    private String state;
+    private String latestTotalCases;
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getLatestTotalCases() {
+        return latestTotalCases;
+    }
+    public void setLatestTotalCases(String latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+
+}
---
+++ src/main/java/covid/tracker/stats/StatsApplication.java
@@ -0,0 +1,18 @@
+package covid.tracker.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class StatsApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StatsApplication.class, args);
+	}
+
+}
